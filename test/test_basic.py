cbc1119658236e8d6e1020a164c87cf39b171eaf
Dan Lecocq <dan@seomoz.org>
2013-10-17 09:40:41 -0700

    Failing test for complete/advance

--- test/test_basic.py
+++ test/test_basic.py
@@ -11,3 +11,11 @@ class TestBasic(TestQsome):
             self.lua('put', 0, 'worker', 'queue', index, 'klass', 5, [], 0)
         results = self.lua('multiget', 1, *range(10))
         self.assertEqual(len(results), 10)
+
+    def test_complete(self):
+        '''We shoudl be able to complete jobs and advance them'''
+        self.lua('put', 0, 'worker', 'queue', 'jid', 'klass', 5, {}, 0)
+        self.assertEqual(len(self.lua('pop', 1, 'queue', 'worker', 10)), 1)
+        self.lua('complete', 2, 'jid', 'worker', 'queue', {}, 'next', 'foo')
+        data = self.lua('get', 3, 'jid')
+        self.assertEqual(data['state'], 'waiting')
